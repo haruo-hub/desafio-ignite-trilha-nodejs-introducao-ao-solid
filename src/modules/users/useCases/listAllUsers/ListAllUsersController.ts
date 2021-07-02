0e15c30d5995ef73928dfee6296b10f79f7f234b
Rodrigo Haruo <rodrigoharuosaitou@gmail.com>
2021-07-02 12:55:06 -0300

    Challenge solution

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -3,11 +3,17 @@ import { Request, Response } from "express";
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
 class ListAllUsersController {
-  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+    constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
-  }
+    handle(request: Request, response: Response): Response {
+        const user_id = request.header("user_id");
+        try {
+            const all = this.listAllUsersUseCase.execute({ user_id });
+            return response.json(all);
+        } catch (error) {
+            return response.status(400).json({ error: error.message });
+        }
+    }
 }
 
 export { ListAllUsersController };
